d16dafca5ef11ac7ca453efae6204055c37e78e1
Shailesh2003-arch <117181669+Shailesh2003-arch@users.noreply.github.com>
2024-11-30 14:19:10 +0530

    update

--- unformattedInputFunc.c
+++ unformattedInputFunc.c
@@ -8,12 +8,17 @@
 // using the getchar() you can take the input of a single character from the user...
 
 int main(){
-    char ch;
-    printf("Enter your preffered character : ");
-    ch = getchar();
-    printf("Your entered Character is: %c",ch);
+    // char ch;
+    // printf("Enter your preffered character : ");
+    /*ch = getchar(); // use getchar to take a single character input from the user...
+    */
+    // printf("Your entered Character is: %c",ch);
+    char str[16];
+    printf("Enter your Preffered String:\n");
+    fgets(str,sizeof(str),stdin);
+    printf("Your entered string is:%s\n",str);
     return 0;
-}
+} 
 
 
 
