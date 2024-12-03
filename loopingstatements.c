f43978373872ec5e004aafcf0ce13636ea004c9d
Shailesh2003-arch <117181669+Shailesh2003-arch@users.noreply.github.com>
2024-12-03 17:30:00 +0530

    update

--- loopingstatements.c
+++ loopingstatements.c
@@ -29,6 +29,8 @@ int main(){
 
 // factorial of a number...
 
+
+/* 
 int main(){
     int fact=1,n;
     printf("Enter the number you want factorial of: ");
@@ -38,4 +40,58 @@ int main(){
     }
     printf("Factorial of the entered number is %d",fact);
 }
+*/
 
+
+// Print all the even No's from 1 to 50
+/*
+int main(){
+    for(int i=1;i<=50;i++){
+        if(i%2==0){
+            printf("%d\n",i);
+        }
+    }
+    return 0;
+}
+*/
+
+
+// Print all the odd No's from 1 to 50
+/*
+int main(){
+    for(int i=1;i<=50;i++){
+        if(i%2!=0){
+            printf("%d\n",i);
+        }
+    }
+    return 0;
+}
+*/
+
+//print all the number fromm n to 1 order
+
+/*
+int main(){
+    int n;
+    printf("Enter the N to start from: ");
+    scanf("%d",&n);
+    for(int i=n;i>=1;i--){
+        printf("%d\n",i);
+    }
+    return 0;
+}
+*/
+
+//Multiplication table
+/*
+int main(){
+    
+    int n;
+    printf("Enter the number you want table of: ");
+    scanf("%d",&n);
+    for(int i=1;i<=10;i++){
+        printf("%d x %d = %d\n",n,i,n*i);
+    }
+    return 0;
+}
+*/
